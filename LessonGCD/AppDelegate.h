4186d1216187dd12df65f4390b700890f3531f7b
NatureZhang <895234387@qq.com>
2016-05-17 10:44:24 +0800

    Initial Commit

---
+++ LessonGCD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LessonGCD
+//
+//  Created by zhangdong on 16/5/17.
+//  Copyright © 2016年 zhangdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
